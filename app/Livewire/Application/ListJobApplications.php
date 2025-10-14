22fec6cabd7f6064f4eab7866679dfffe5076995
Pranay Teja Baddam <pranay.baddam@bigtincan.com>
2025-10-14 09:29:17 +0100

    Working on job applications page.

--- app/Livewire/Application/ListJobApplications.php
+++ app/Livewire/Application/ListJobApplications.php
@@ -1,13 +1,41 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Livewire\Application;
 
+use App\Models\JobApplication;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\View\View;
 use Livewire\Component;
+use Livewire\WithPagination;
 
-class ListJobApplications extends Component
+/**
+ * @property LengthAwarePaginator|JobApplication[] $applications
+ */
+final class ListJobApplications extends Component
 {
-    public function render()
+    use WithPagination;
+
+    public array $selectedApplicationIds = [];
+
+    public array $applicationIdsOnPage = [];
+
+    public array $applicationIds = [];
+
+    public array $filters = [];
+
+    public int $activeFilterCount = 0;
+
+    public string $search = '';
+
+    public string $sortCol = '';
+
+    public string $sortDirection = '';
+
+    public function render(): View
     {
-        return view('livewire.application.list-job-applications');
+        return view('livewire.application.list-job-applications')
+            ->title(config('app.name').' | List Applications');
     }
 }
